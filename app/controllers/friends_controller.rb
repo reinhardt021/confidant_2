1a27882143d216838d891ed796ee7eb21b3eaff6
reinhardtcgr <reinhardt.cgr@gmail.com>
2016-03-20 19:23:26 -0700

    Added forms to the page and fixed up some styling

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,15 @@
 class FriendsController < ApplicationController
   def index
     @friends = Friend.all
+    @friend = Friend.new
+  end
+
+  def destroy
+    @friend = Friend.find(params[:id])
+    @friend.destroy
+
+    flash[:notice] = "#{@friend.username} deleted from database"
+    redirect_to friends_path
   end
 
 end
--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,10 @@
 class Friend < ActiveRecord::Base
+  validates :first_name, 
+    presence: true 
+  validates :last_name, 
+    presence: true 
+  validates :username, 
+    presence: true,
+    uniqueness: true
+
 end
