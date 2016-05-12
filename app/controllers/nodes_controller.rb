73ed67d713221d09e6325c50b8357d517799bcf3
reinhardtcgr <reinhardt.cgr@gmail.com>
2016-05-11 23:55:48 -0700

    Added a form to create new nodes easily

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -4,4 +4,19 @@ class NodesController < ApplicationController
     @node = Node.new
   end
 
+  def create
+    @node = Node.new(node_params)
+
+    if @node.save
+      redirect_to nodes_path, notice: "#{@node.title} was submitted successfully!"
+    else
+      render :index
+    end
+  end
+
+  protected
+
+  def node_params
+    params.require(:node).permit(:title, :node_type, :content)
+  end
 end
