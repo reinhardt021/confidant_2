c4103192508b0bdd71b880c749212b70bfb23e75
reinhardtcgr <reinhardt.cgr@gmail.com>
2016-05-11 19:51:51 -0700

    Was able to move the divs around using jquery, just need to store into database now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,80 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function () {
+  console.log('hello wisconsin');
+  var el = null;
+  var mousePosition = { 
+    x: -1, 
+    y: -1,
+    last: {
+      x: -1, 
+      y: -1
+    }
+  };
+  var elPosition = {
+    x: -1,
+    y: -1,
+    last: {
+      x: -1,
+      y: -1
+    }
+  };
+
+  $(document).mousemove(function (event) {
+    mousePosition.last.x = mousePosition.x;
+    mousePosition.last.y = mousePosition.y;
+    mousePosition.x = event.pageX;
+    mousePosition.y = event.pageY;
+    console.log('the cursor coordinates moved from { x: ' + mousePosition.last.x +
+      ', y: ' + mousePosition.last.y + ' } >>> { x: ' + mousePosition.x +
+      ', y: ' + mousePosition.y + ' }'
+    );
+
+    if (el) {
+      elPosition.last.x = elPosition.x;
+      elPosition.last.y = elPosition.y;
+      elPosition.x = elPosition.x + (mousePosition.x - mousePosition.last.x);
+      elPosition.y = elPosition.y + (mousePosition.y - mousePosition.last.y);
+
+      el.offset({ 
+        left: elPosition.x,
+        top: elPosition.y 
+      });
+
+      console.log(
+        'the element coordinates moved from { x: ' + mousePosition.last.x +
+        ', y: ' + mousePosition.last.y + 
+        ' } >>> { x: ' + mousePosition.x +
+        ', y: ' + mousePosition.y + ' }'
+      );
+    }
+  });
+
+  $('div').mousedown(function (event) {
+    event.stopPropagation();
+    el = $(this);
+    var position = el.offset();
+
+    elPosition.x = position.left;
+    elPosition.y = position.top;
+  });
+
+  $('div').mouseup(function (event) {
+    event.stopPropagation();
+    console.log('mouseup event');
+
+    elPosition = {
+      x: -1,
+      y: -1,
+      last: {
+        x: -1,
+        y: -1
+      }
+    };
+
+    el = null;
+  });
+
+});
