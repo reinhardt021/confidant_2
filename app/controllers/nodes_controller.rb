91519ea45017b4234dae0ace9fdf52060b63d764
reinhardtcgr <reinhardt.cgr@gmail.com>
2016-05-23 20:44:23 -0700

    Got the positions to save into database but just have issue with too many redirects which I must figure out

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -18,19 +18,47 @@ class NodesController < ApplicationController
     puts ">>>> params[ajaxNodes] >>>>"
     puts params["ajaxNodes"]
 
+    updated = true
+
+    params["ajaxNodes"].each do |key, node_data|
+      # go into each node and update each value
+      node = Node.find(key.to_i)
+      # node.title
+      # node.content
+      # node.node_type
+      node.position_x = node_data["x"]
+      node.position_y = node_data["y"]
+      # then save for each node
+      updated = false if !node.save
+    end
+
+    puts Node.all
+    # {
+    #   "1"=>{
+    #     "id"=>"1", 
+    #     "title"=>"wake", 
+    #     "content"=>"slowly get out of bed", 
+    #     "type"=>"action", 
+    #     "x"=>"-1", 
+    #     "y"=>"-1"
+    #   }, ...
+    # }
+
     # @node = Node.find(params[:id])
 
     # the following line appears to be unable to find the :node
     #  thought it was an issue of the validations which i commented out
     #  still doesn't work and not sure why
 
+    if updated
     # if @node.update_attributes(node_params)
     #   # might have to get rid of this is doing AJAX calls
-    #   redirect_to nodes_path(@node), notice: "#{@node.title} was updated successfully!"
-    # else
+      redirect_to nodes_path
+      # redirect_to nodes_path, notice: "nodes were updated successfully!"
+    else
     #   # same here
-    #   render :index
-    # end
+      render :index
+    end
   end
 
   protected
